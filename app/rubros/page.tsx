9b1fde0bc585786f82b093fb2298a81cd4249ce0
Agustin-Monti <153923960+Agustin-Monti@users.noreply.github.com>
2025-03-27 13:02:03 -0300

    Create page.tsx

---
+++ app/rubros/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import HeaderAdmin from "@/components/HeaderAdmin";
+import Sidebar from "@/components/Sidebar";
+
+
+
+export default function RubrosPage() {
+
+
+return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      
+      <div className="flex-1 ml-64">
+        <HeaderAdmin />
+        
+        <div className="p-6">
+          <h1 className="text-2xl font-semibold mb-6">Proximamente...</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
