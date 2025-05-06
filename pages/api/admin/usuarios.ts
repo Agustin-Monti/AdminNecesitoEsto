8f7b026300c2f18e6364a460e73dbed385c51cb4
Agustin-Monti <153923960+Agustin-Monti@users.noreply.github.com>
2025-05-06 10:02:00 -0300

    Update usuarios.ts

--- pages/api/admin/usuarios.ts
+++ pages/api/admin/usuarios.ts
@@ -5,7 +5,12 @@ import supabaseAdmin from '@/lib/supabaseAdmin';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { data, error } = await supabaseAdmin
     .from('profile')
-    .select('*'); // trae todos los perfiles
+    .select(`
+      *,
+      categorias (
+        categoria
+      )
+    `); // trae todos los perfiles
 
   if (error) {
     return res.status(500).json({ message: 'Error al traer usuarios', error });
