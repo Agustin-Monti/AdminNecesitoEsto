38c3c96eae78f4a7f742496a7792d8ec46484659
Agustin-Monti <153923960+Agustin-Monti@users.noreply.github.com>
2025-10-31 10:41:30 -0300

    Add files via upload

---
+++ app/envios/page.tsx
@@ -0,0 +1,22 @@
+// app/home/page.tsx
+"use client";
+
+import HeaderAdmin from "@/components/HeaderAdmin";
+import Sidebar from "@/components/Sidebar";
+import EnvioMails from "@/components/envios";
+
+export default function HomePage() {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      
+      <div className="flex-1 ml-64">
+        <HeaderAdmin />
+        
+        <div className="p-6">
+          <EnvioMails />
+        </div>
+      </div>
+    </div>
+  );
+}
